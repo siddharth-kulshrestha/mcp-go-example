862d1e5347ac41c43df986f7e9c437097bc7f1c4
Siddharth Kulshrestha <kulshresthasiddharth96@gmail.com>
2026-01-23 20:23:43 +0530

    Added Client code to talk to MCP TODO: 1. Complete implementation of client. 2. Get and expose one Gemini key

--- weather_app/client/client.go
+++ weather_app/client/client.go
@@ -1,3 +1,72 @@
 package client
 
-func StartClient() {}
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/googleai"
+)
+
+var GeminiAPIKey string
+
+func init() {
+	GeminiAPIKey = os.Getenv("GEMINI_API_KEY")
+}
+
+func StartClient() error {
+
+	ctx := context.Background()
+
+	mcpClient := mcp.NewClient(&mcp.Implementation{Name: "weather_client", Version: "0.0.0"}, nil)
+	transport := &mcp.CommandTransport{Command: exec.Command("./weather_server")}
+	session, err := mcpClient.Connect(ctx, transport, nil)
+	if err != nil {
+		return err
+	}
+	defer session.Close()
+
+	var serverTools []llms.Tool
+	mcpTools, err := session.ListTools(ctx, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Println(mcpTools)
+
+	for _, tool := range mcpTools.Tools {
+		serverTools = append(serverTools, llms.Tool{
+			Type: "function",
+			Function: &llms.FunctionDefinition{
+				Name:        tool.Name,
+				Description: tool.Description,
+				Parameters:  tool.InputSchema,
+			},
+		})
+	}
+
+	// Setup Gemini LLM
+	llm, err := googleai.New(ctx, googleai.WithAPIKey(GeminiAPIKey))
+	if err != nil {
+		return err
+	}
+
+	// Agent state
+	initialContext := []llms.MessageContent{llms.TextParts(llms.ChatMessageTypeSystem, "You are a helpful assistant using tools for weather.")}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("--- Weather Agent Ready (Go) ---")
+
+	for {
+		fmt.Printf("\nUser: ")
+		if !scanner.Scan() {
+			break
+		}
+		input := scanner.Text()
+
+	}
+
+}
--- weather_app/go.mod
+++ weather_app/go.mod
@@ -6,6 +6,7 @@ require github.com/modelcontextprotocol/go-sdk v1.2.0
 
 require (
 	github.com/google/jsonschema-go v0.3.0 // indirect
+	github.com/tmc/langchaingo v0.1.14 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 )
